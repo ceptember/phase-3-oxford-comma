96c71ed9e4d4adfb9a5dc9114e051ae0883c99a0
Christy Perozzi <christy.perozzi@gmail.com>
2022-02-04 11:34:26 -0500

    commit tree 88037e244e2ac22e2037ccc5ffa883c8b3827b85

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    if array.length == 1 
+        array[0]
+    elsif array.length == 2 
+        "#{array[0]} and #{array[1]}"
+    else 
+        array[array.length-1] = "and #{array[array.length-1]}"
+        array.join(", ")
+    end
 end
+
+arr1 = ["fiddleheads"]
+arr2 = ["fiddleheads", "okra"]
+arr3 = ["fiddleheads", "okra", "kohlrabi"]
+arr4 = ["fiddleheads", "okra", "kohlrabi", "kale", "carrots", "squash", "onions"]
+puts oxford_comma(arr4)
